14cfb0db8525cdb2595f10e7af8d2d5b41876b91
bil0ak <bilal0akkil@gmail.com>
2023-01-25 10:54:33 +0400

    switch to firestore, finish style

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 
 function App() {
+  // change title
+  document.title = "Todo App | By Bilal Akkil";
   return (
     <Routes>
       <Route path="/" element={<Home />} />
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,23 +1,15 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useState } from "react";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
-import { app } from "../../config/firebase-config";
+import { app, removeTodo } from "../../config/firebase-config";
 import { writeTodoData } from "../../config/firebase-config";
-import { getDatabase, onValue, ref } from "firebase/database";
-
-const todos = [
-  {
-    id: 1,
-    name: "todo 1",
-  },
-  {
-    id: 2,
-    name: "todo 2",
-  },
-  {
-    id: 3,
-    name: "todo 3",
-  },
-];
+import {
+  collection,
+  getFirestore,
+  onSnapshot,
+  query,
+} from "firebase/firestore";
+import "./home.css";
 
 export default function Home() {
   const [user, setUser] = useState(null);
@@ -27,6 +19,7 @@ export default function Home() {
       if (user) {
         // User is signed in, see docs for a list of available properties
         // https://firebase.google.com/docs/reference/js/firebase.User
+        // eslint-disable-next-line no-unused-vars
         const uid = user.uid;
         // ...
         setUser(user);
@@ -41,40 +34,32 @@ export default function Home() {
 
   const [data, setData] = useState([]);
 
-  const db = getDatabase();
-  //   useEffect(() => {
-  //     setData(todos);
-  //   }, []);
+  const db = getFirestore(app);
 
   useEffect(() => {
-    user &&
-      onValue(ref(db, "users/" + user.uid + "/todos"), (snapshot) => {
-        const data = snapshot.val();
-        //   console.log(data);
-        //   convert object of objects to array of objects
-        const dataArr = Object.keys(data).map((key) => {
-          return { ...data[key], id: key };
+    const getData = async () => {
+      const q = query(collection(db, "users/" + user.uid + "/todos"));
+      // eslint-disable-next-line no-unused-vars
+      const unsubscribe = onSnapshot(q, (querySnapshot) => {
+        const data = [];
+        querySnapshot.forEach((doc) => {
+          data.push({ ...doc.data(), id: doc.id });
         });
-        console.log(dataArr);
-        setData(dataArr);
+        setData(data);
       });
+    };
+    user && getData();
   }, [user]);
-
-  //   const user_todos = ref(db, "users/" + user.uid + "/todos");
-
-  //   onValue(user_todos, (snapshot) => {
-  //     const data = snapshot.val();
-  //     console.log(data);
-  //     setData(data);
-  //   });
 
   const handleDeleteBtnClick = (e, id) => {
     e.preventDefault();
-    const newData = data.filter((item) => item.id !== id);
-    setData(newData);
+    let confirmation = window.confirm("Are you sure you want to delete this?");
+    if (confirmation) {
+      removeTodo(user.uid, id);
+    }
   };
 
-  const [newTodo, setNewTodo] = useState();
+  const [newTodo, setNewTodo] = useState("");
 
   const handleNewTodoChange = (e) => {
     setNewTodo(e.target.value);
@@ -82,29 +67,67 @@ export default function Home() {
 
   const handleAddBtnClick = (e) => {
     e.preventDefault();
+    if (newTodo === "") {
+      return;
+    }
     writeTodoData(user.uid, newTodo);
+    // empty input
+    setNewTodo("");
   };
+
+  const handleSignOut = () => {
+    auth.signOut();
+  };
+
   return (
-    <div>
-      <h1>Home</h1>
-      <p>data:</p>
-      <div>
-        <ul>
-          {data &&
-            data.map((item) => (
-              <li key={item.id}>
-                <p>{item.title}</p>
-                <p>{new Date(item.date).toLocaleString()}</p>
-                <button onClick={(e) => handleDeleteBtnClick(e, item.id)}>
-                  Delete
-                </button>
-              </li>
-            ))}
-        </ul>
+    <div className="home">
+      <h1>TODOS:</h1>
+      <div className="row">
+        <div className="left">
+          <div className="add_todo_container">
+            <form>
+              <input
+                type="text"
+                onChange={(e) => handleNewTodoChange(e)}
+                value={newTodo}
+                className="add_todo_input"
+                placeholder="Add Todo"
+              />
+              <button
+                onClick={(e) => handleAddBtnClick(e)}
+                className="add_todo_btn"
+                type="submit"
+              >
+                Add Todo
+              </button>
+            </form>
+          </div>
+          <br />
+          <button onClick={handleSignOut} className="sign_out_btn">
+            Sign Out
+          </button>
+        </div>
+
+        <div className="right">
+          <ul className="todos_list">
+            {data &&
+              data.map((item) => (
+                <li key={item.id} className="todo_item">
+                  <p className="todo_item_title">{item.title}</p>
+                  <p className="todo_item_date">
+                    {new Date(item.date).toLocaleString()}
+                  </p>
+                  <button
+                    onClick={(e) => handleDeleteBtnClick(e, item.id)}
+                    className="todo_item_delete"
+                  >
+                    Delete
+                  </button>
+                </li>
+              ))}
+          </ul>
+        </div>
       </div>
-      <br />
-      <input type="text" onChange={(e) => handleNewTodoChange(e)} />
-      <button onClick={(e) => handleAddBtnClick(e)}>Add</button>
     </div>
   );
 }
