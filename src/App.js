1d7a9e48c0c156dd0590ad4670c842b3ceede7bc
bil0ak <bilal0akkil@gmail.com>
2023-02-03 01:44:40 +0400

    check if user if logged in before loading home page

--- src/App.js
+++ src/App.js
@@ -3,13 +3,27 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import MagicUrl from "./pages/magicUrl/MagicUrl";
+import { getAccount } from "./config/appwrite-config";
+import { useState } from "react";
 
 function App() {
   // change title
   document.title = "Todo App | By Bilal Akkil";
+
+  const [user, setUser] = useState(null);
+  const promise = getAccount();
+  promise.then(
+    function (response) {
+      setUser(response);
+    },
+    function (error) {
+      console.log(error);
+    }
+  );
+
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={user ? <Home /> : <Login />} />
       <Route path="/login" element={<Login />} />
       <Route path="/magicURL" element={<MagicUrl />} />
     </Routes>
